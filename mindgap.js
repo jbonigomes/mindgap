5f05901a45ac9144685bb67bcf9f726c82bcdf71
Jose B Gomes <jose@believecreative.com>
2015-11-17 22:40:45 +0000

    Adding dynamic list

--- mindgap.js
+++ mindgap.js
@@ -1,15 +1,20 @@
+Items = new Mongo.Collection('items');
+
 if(Meteor.isClient) {
   Template.body.helpers({
-    items: [
-      { title: 'Change bed sheets', recurring: 'week', number: 2, time: 555 },
-      { title: 'Take medication', recurring: 'week', number: 2, time: 555 },
-      { title: 'Change tooth brush', recurring: 'week', number: 2, time: 555 },
-      { title: 'Change bed sheets', recurring: 'week', number: 2, time: 555 },
-      { title: 'Take medication', recurring: 'week', number: 2, time: 555 },
-      { title: 'Change tooth brush', recurring: 'week', number: 2, time: 555 },
-      { title: 'Change bed sheets', recurring: 'week', number: 2, time: 555 },
-      { title: 'Take medication', recurring: 'week', number: 2, time: 555 },
-      { title: 'Change tooth brush', recurring: 'week', number: 2, time: 555 }
-    ]
+    items: function () {
+      return Items.find({});
+    }
+    // items: [
+    //   { title: 'Change bed sheets', recurring: 'week', number: 2, time: 555 },
+    //   { title: 'Take medication', recurring: 'week', number: 2, time: 555 },
+    //   { title: 'Change tooth brush', recurring: 'week', number: 2, time: 555 },
+    //   { title: 'Change bed sheets', recurring: 'week', number: 2, time: 555 },
+    //   { title: 'Take medication', recurring: 'week', number: 2, time: 555 },
+    //   { title: 'Change tooth brush', recurring: 'week', number: 2, time: 555 },
+    //   { title: 'Change bed sheets', recurring: 'week', number: 2, time: 555 },
+    //   { title: 'Take medication', recurring: 'week', number: 2, time: 555 },
+    //   { title: 'Change tooth brush', recurring: 'week', number: 2, time: 555 }
+    // ]
   });
 }
