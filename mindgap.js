d792c328cfbce739e381d6b100086735a591a1e7
Jose B Gomes <jose@believecreative.com>
2015-11-18 23:54:03 +0000

    Adding simple events

--- mindgap.js
+++ mindgap.js
@@ -1,5 +1,8 @@
 if(Meteor.isClient) {
 
+  var animationEvent = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd ' +
+    'oanimationend animationend';
+
   var items = new Ground.Collection(null);
 
   Template.body.helpers({
@@ -17,28 +20,41 @@ if(Meteor.isClient) {
       var number = $('input[type="range"]').val();
       var reocur = $('.icons .active').data('value');
 
-      // Clear the elements
-      $('input[type="text"]').val('');
-      $('input[type="range"]').val('');
-      $('.icons i').each(function(i) {
-        if(i === 0) {
-          $(this).addClass('active');
-        }
-        else {
-          $(this).removeClass('active');
-        }
-      });
+      if(text.length > 1) {
+        // Clear the elements
+        $('input[type="text"]').val('');
+        $('input[type="range"]').val('');
+        $('.icons i').each(function(i) {
+          if(i === 0) {
+            $(this).addClass('active');
+          }
+          else {
+            $(this).removeClass('active');
+          }
+        });
 
-      // Insert on DB
-      items.insert({
-        title: text,
-        number: +number,
-        recurring: reocur,
-        time: new Date().getTime()
-      });
+        // Insert on DB
+        items.insert({
+          title: text,
+          number: +number,
+          recurring: reocur,
+          time: new Date().getTime()
+        });
 
-      // Hide the form
-      $('body').removeClass('editing');
+        // Hide the form
+        $('body').removeClass('editing');
+      }
+      else {
+        $('.save-button').addClass('shake animated');
+        $('.save-button').on(animationEvent, function() {
+          $('.save-button').removeClass('shake animated');
+        });
+
+        $('input[type="text"]').addClass('shake animated');
+        $('input[type="text"]').on(animationEvent, function() {
+          $('input[type="text"]').removeClass('shake animated');
+        });
+      }
     },
     'click .new-button': function(e) {
       e.preventDefault();
